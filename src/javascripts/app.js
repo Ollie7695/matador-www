e0f35e3406cdc0feddcba9adad444a1d72e97169
Ollie <ollie@wodco.com>
2019-02-03 14:52:38 +0000

    Article

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -2,6 +2,7 @@ var $ = require('jquery');
 var Foundation = require('foundation-sites');
 var gsap = require("gsap");
 var TweenMax = require('gsap').TweenMax;
+import Headroom from "headroom.js";
 
 TweenMax.staggerFromTo(
 	".masthead h1, .masthead p, .masthead a img", 1, {
@@ -14,3 +15,29 @@ TweenMax.staggerFromTo(
 	}, .1);
 
 $(document).foundation();
+
+if (document.querySelector('.scrolling-nav')) {
+	// Scrolling article nav
+	const headroom = new Headroom(document.querySelector(".scrolling-nav"), {
+		offset: 100,
+		tolerance: {
+			up: 100,
+			down: 0
+		}
+	});
+	headroom.init();
+
+	// Scrolling progress bar
+	var winHeight = $(window).height(), 
+		docHeight = $(".article").height() + 650,
+		progressBar = $('progress'),
+		max, value;
+
+		max = docHeight - winHeight;
+		progressBar.attr('max', max);
+
+	$(document).on('scroll', function(){
+		value = $(window).scrollTop();
+		progressBar.attr('value', value);
+	});
+}
