b00ab0014361d84686160afb6ab206099aadea93
Ollie B <ollie@wodco.com>
2019-02-09 17:40:08 +0000

    Homepage updates

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -2,6 +2,7 @@ var $ = require('jquery');
 var Foundation = require('foundation-sites');
 var gsap = require("gsap");
 var TweenMax = require('gsap').TweenMax;
+var Flickity = require("flickity");
 
 TweenMax.staggerFromTo(
 	".masthead h1, .masthead p, .masthead a img", 1, {
@@ -12,5 +13,12 @@ TweenMax.staggerFromTo(
 		y: 0,
 		ease: Power2.easeInOut
 	}, .1);
+
+var flkty = new Flickity( '.mobile-features', {
+	prevNextButtons: false,
+	pageDots: false,
+	// adaptiveHeight: true,
+	// cellAlign: 'left'
+});
 
 $(document).foundation();
